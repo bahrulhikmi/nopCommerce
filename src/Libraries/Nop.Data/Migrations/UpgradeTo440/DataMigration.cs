06ce5238ae829a6eb9c09ec91d8c6abe9d3670e3
skoshelev <sergey.k@nopcommerce.com>
2020-07-02 15:10:09 +0300

    4839 Stop using the upgrade.sql

---
+++ src/Libraries/Nop.Data/Migrations/UpgradeTo440/DataMigration.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+using Nop.Core;
+
+namespace Nop.Data.Migrations.UpgradeTo440
+{
+    [NopMigration(NopVersion.FULL_VERSION, MigrationType.Data, "version 4.40. Update data on the database")]
+    public class DataMigration : MigrationBase
+    {      
+        /// <summary>
+        /// Collect the UP migration expressions
+        /// </summary>
+        public override void Up()
+        {
+            #if DEBUG
+            throw new NopException("Prevention of fix this migration in the database while development is in progress");
+            #endif
+        }
+
+        public override void Down()
+        {
+            //add the downgrade logic if necessary 
+        }
+    }
+}
